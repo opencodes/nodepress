877993b12aad4cc045133cb3dad978f5ff4855b8
rajesh <rajesh.jha@urbantouch.com>
2012-05-16 14:47:39 +0530

    add this

---
+++ config/admin.js
@@ -0,0 +1,12 @@
+var config = {
+  port: 1987,
+  cdn: 'http://localhost:3000',
+  db_options: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'nodepress'
+  }	
+}
+module.exports = config;
